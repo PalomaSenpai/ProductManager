c85ecf6621ffe1059492ab40dab4b38600fd989e
PalomaSenpai <AguilaProPlayer@gmmail.com>
2024-12-09 21:19:50 -0600

    Cambios en el server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import router from './router'
 import db from './config/db'
 import colors from 'colors'
+import cors, {CorsOptions} from 'cors'
+import morgan from 'morgan'
 import swaggerUI from 'swagger-ui-express'
 import swaggerSpec, {swaggerUiOptions} from './config/swagger'
 
@@ -21,10 +23,24 @@ connectDB()
 //Instancia de express
 const server = express()
 
+//permitiy conexiones
+const corsOptions: CorsOptions = {
+    origin: function (origin, callback) {
+        if(origin === process.env.FRONTEND_URL || origin==='http://localhost:4000/'){
+            callback(null,true)
+        }else{
+            callback(new Error('Error de CORS'))
+        }
+    }
+}
+
+server.use(cors(corsOptions));
+
+
 //Leer datos de formularios
 server.use(express.json())
 
-
+server.use(morgan('dev'))
 server.use('/api/products', router)
 
 //Docs
